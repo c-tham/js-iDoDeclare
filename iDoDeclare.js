b78d785f5102ba329b782f03ec9c51e99ed66e5e
Chia T <tham_oh@hotmail.com>
2017-12-12 22:07:30 -0600

    init

---
+++ iDoDeclare.js
@@ -0,0 +1,18 @@
+console.log('The JavaScript World');
+var num1 = 10000000000000000000000000;
+var num2 = -0.999999999999999999999999;
+var num3 = 123456789.0987654321;
+var num4 = num1 - num2;
+var text1 = 'CodingDojo';
+var text2 = 'Elmo';
+var text3 = 'Oklahoma';
+var boo1 = true;
+var boo2 = false;
+var undefine;
+console.log('Hello my friend, '+text2+'. Welcome to '+text1+' at '+text3);
+console.log('what resule of '+num1+' subtract '+num2+' is?');
+console.log('The answer is not '+num3);
+console.log('The answer is '+num4);
+console.log('The undefined variable is ('+undefine+').');
+console.log('The true is ('+boo1+') and false is ('+boo2+').');
+console.log('The End!');
